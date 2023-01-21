346a4b0f6f30f973e3a3e53a7d7269bbb9987e7c
rrauf8178 <rrauf8178@gmail.com>
2023-01-21 17:20:40 +0400

    created shoppingcart proje and added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BsBasket3Fill } from "react-icons/bs";
+
+
+function Home() {
+    return (
+        <header>
+        <div className="overlay"></div>
+          <nav>
+            <h2>Shop</h2>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li className="cart">
+                <a href="#"><BsBasket3Fill className="basket-icon" size={20}/>Cart<span>0</span></a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+    )
+}
+
+export default Home;
